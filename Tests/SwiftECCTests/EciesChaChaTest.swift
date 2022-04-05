af5350d31d5cd6609a55f4ec2258feb47c2da1b7
Leif Ibsen <leif.m.ibsen@gmail.com>
2022-04-05 16:58:02 +0200

    Release 3.3.0

---
+++ Tests/SwiftECCTests/EciesChaChaTest.swift
@@ -0,0 +1,70 @@
+//
+//  EciesChaChaTest.swift
+//  SwiftECCTests
+//
+//  Created by Leif Ibsen on 04/04/2022.
+//
+
+import XCTest
+
+class EciesChaChaTest: XCTestCase {
+
+    let message = Bytes("The quick brown fox jumps over the lazy dog".utf8)
+    let aad = Bytes("This is the Additional Authenticated Data".utf8)
+
+    func randomBytes(_ bytes: inout Bytes) {
+        guard SecRandomCopyBytes(kSecRandomDefault, bytes.count, &bytes) == errSecSuccess else {
+            fatalError("randomBytes failed")
+        }
+    }
+
+    func doTest1(_ pub: ECPublicKey, _ priv: ECPrivateKey) {
+        do {
+            let encrypted = pub.encryptChaCha(msg: message)
+            let decrypted = try priv.decryptChaCha(msg: encrypted)
+            XCTAssertEqual(message, decrypted)
+        } catch {
+            XCTFail("\(error)")
+        }
+        do {
+            let encrypted = pub.encryptChaCha(msg: [])
+            let decrypted = try priv.decryptChaCha(msg: encrypted)
+            XCTAssertEqual([], decrypted)
+        } catch {
+            XCTFail("\(error)")
+        }
+        do {
+            let encrypted = pub.encryptChaCha(msg: message, aad: aad)
+            let decrypted = try priv.decryptChaCha(msg: encrypted, aad: aad)
+            XCTAssertEqual(message, decrypted)
+        } catch {
+            XCTFail("\(error)")
+        }
+        do {
+            let encrypted = pub.encryptChaCha(msg: [], aad: aad)
+            let decrypted = try priv.decryptChaCha(msg: encrypted, aad: aad)
+            XCTAssertEqual([], decrypted)
+        } catch {
+            XCTFail("\(error)")
+        }
+        do {
+            var randomMessage = Bytes(repeating: 0, count: 1000)
+            randomBytes(&randomMessage)
+            var randomAAD = Bytes(repeating: 0, count: 100)
+            randomBytes(&randomAAD)
+            let encrypted = pub.encryptChaCha(msg: randomMessage, aad: randomAAD)
+            let decrypted = try priv.decryptChaCha(msg: encrypted, aad: randomAAD)
+            XCTAssertEqual(randomMessage, decrypted)
+        } catch {
+            XCTFail("\(error)")
+        }
+    }
+
+    func test() {
+        for c in ECCurve.allCases {
+            let (pub, priv) = Domain.instance(curve: c).makeKeyPair()
+            doTest1(pub, priv)
+        }
+    }
+
+}
