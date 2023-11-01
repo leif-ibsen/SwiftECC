7cfbb38fff5dbd602c301f7014b267a0bf3dd687
John <jhntipo@gmail.com>
2023-11-01 19:21:21 +0300

    add swift-crypto

--- Package.swift
+++ Package.swift
@@ -16,13 +16,18 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/leif-ibsen/BigInt", from: "1.13.0"),
         .package(url: "https://github.com/leif-ibsen/ASN1", from: "2.1.0"),
+        .package(url: "https://github.com/apple/swift-crypto", "1.0.0" ..< "3.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftECC",
-            dependencies: ["BigInt", "ASN1"]),
+            dependencies: [
+                "BigInt",
+                "ASN1",
+                .product(name: "Crypto", package: "swift-crypto", condition: .when(platforms: [.linux])),
+        ]),
         .testTarget(
             name: "SwiftECCTests",
             dependencies: ["SwiftECC"]),
